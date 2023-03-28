8eaacec94471b965b853be79b5cc08b4bf12b66e
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-03-28 16:31:29 -0400

    valid anagrams(not done) oddevensort (done)

--- custom-sorts.js
+++ custom-sorts.js
@@ -4,11 +4,30 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    if(a%2 === 1 && b%2 ===1){
+      return a-b;
+    }
+    if(a%2 === 0 && b%2 ===0){
+      return a-b;
+    }
+    if(a%2 === 1){
+      return -1;
+    }
+    return 1;
+  })
 }
-
 function validAnagrams(s, t) {
-  // Your code here
+  s = s.split("");
+  t = t.split("");
+  let sret;
+  let tret;
+  sret = s.sort((a,b) => parseInt(a) - parseInt(b));
+  tret = t.sort((a,b) => parseInt(a) - parseInt(b));
+  sret = sret.join("");
+  tret = tret.join("");
+  console.log(sret, tret);
+  return sret === tret;
 }
 
 function reverseBaseSort(arr) {
